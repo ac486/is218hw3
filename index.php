b9744682bb3af25e68dc67872dd79ced61541e9c
Arthur Chen <ac486@njit.edu>
2015-05-04 17:56:08 -0400

    added query questions

--- index.php
+++ index.php
@@ -35,7 +35,16 @@ $sql1 = 'select employees.emp_no, employees.first_name, employees.last_name, sal
 foreach($db->query($sql1) as $row) {
    print_r($row);
 }
-
+echo "</br>";
+echo "2. Who is the highest paid employee between 1985 and 1981?</br>";
+echo "3. Which department currently has highest paid department manager?</br>";
+echo "4. What are the titles of all the departments?</br>";
+echo "5. Who are the current department heads?</br>";
+echo "6. Who is highest paid employee that is not a department head?</br>";
+echo "7. Who is currently the lowest paid employee?</br>";
+echo "8. How many employees currently work in each department?</br>";
+echo "9. How much does each department currently spend on salaries?</br>";
+echo "10. How much is currently spent for all salaries?</br>";
 ?>
 </body>
 </html>

52f1a5b26398069504a8a093278e271fef0c6eea
Arthur Chen <ac486@njit.edu>
2015-05-06 18:51:55 -0400

    rough display of all queries

--- index.php
+++ index.php
@@ -54,27 +54,62 @@ foreach($db->query($sql3) as $row){
    echo $row[0].' '.$row[1].' '.$row[2];
 }
 echo "</br></br>";
+
 echo "4. What are the titles of all the departments?</br>";
+echo "</br>";
 $sql4 = 'select * from departments order by departments.dept_no limit 9';
+foreach($db->query($sql4) as $row){
+   echo $row[0].' '.$row[1].'</br>';
+}
+echo"</br></br>";
 
 echo "5. Who are the current department heads?</br>";
+echo "</br>";
 $sql5 = 'select employees.emp_no, employees.first_name, employees.last_name, departments.dept_no, departments.dept_name, dept_manager.from_date, dept_manager.to_date from employees left join dept_manager on employees.emp_no=dept_manager.emp_no, departments where departments.dept_no=dept_manager.dept_no order by dept_manager.to_date desc limit 9';
+foreach($db->query($sql5) as $row){
+   echo $row[0].' '.$row[1].' '.$row[2].' '.$row[3].' '.$row[4].' '.$row[5].' '.$row[6].'</br>';
+}
+echo"</br></br>";
 
 echo "6. Who is highest paid employee that is not a department head?</br>";
+echo"</br>";
 $sql6 = 'select salaries.emp_no, employees.first_name, employees.last_name, salaries.salary from salaries left join employees on salaries.emp_no=employees.emp_no where salaries.emp_no not in (select dept_manager.emp_no from dept_manager) order by salary desc limit 1';
+foreach($db->query($sql6) as $row){
+   echo $row[0].' '.$row[1].' '.$row[2];
+}
+echo "</br></br>";
 
 echo "7. Who is currently the lowest paid employee?</br>";
+echo "</br>";
 $sql7 = 'select employees.emp_no, employees.first_name, employees.last_name, salaries.salary, salaries.from_date, salaries.to_date from employees left join salaries on employees.emp_no=salaries.emp_no order by salaries.to_date desc, salaries.salary ASC limit 1';
+foreach($db->query($sql7) as $row){
+   echo $row[0].' '.$row[1].' '.$row[2];
+}
+echo "</br></br>";
 
 echo "8. How many employees currently work in each department?</br>";
+echo"</br>";
 $sql8 = 'select dept_emp.dept_no, count(dept_emp.emp_no) as total_emp, departments.dept_name  from dept_emp left join departments on dept_emp.dept_no = departments.dept_no where dept_emp.to_date=\'1999-01-01\' group by dept_emp.dept_no limit 9';
+foreach($db->query($sql8) as $row){
+   echo $row[0].' '.$row[1].' '.$row[2];
+}
+echo "</br></br>";
 
 echo "9. How much does each department currently spend on salaries?</br>";
+echo "</br>";
 $sql9 = 'select dept_emp.dept_no, sum(salaries.salary) as total_spent from dept_emp left join salaries on dept_emp.emp_no = salaries.emp_no where dept_emp.to_date=\'1999-01-01\' group by dept_emp.dept_no';
+foreach($db->query($sql9) as $row){
+   echo $row[0].' '.$row[1].'</br>';
+}
+echo "</br></br>";
 
 echo "10. How much is currently spent for all salaries?</br>";
+echo "</br>";
 $sql10 = 'select sum(salaries.salary) as total_spent from salaries where salaries.to_date=\'1999-01-01\'';
-
+foreach($db->query($sql10) as $row){
+   echo $row[0].'</br>';
+}
+echo "</br></br>";
 ?>
 </body>
 </html>

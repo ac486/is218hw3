c9b96ce3710392e09788650d1be9e674017133d0
Arthur Chen <ac486@njit.edu>
2015-05-07 21:39:36 -0400

    fixed fields

--- edit.php
+++ edit.php
@@ -1,9 +1,25 @@
 <form action="action_page.php">
-First name:<br>
-<input type="text" name="firstname" value="Mickey">
 <br>
-Last name:<br>
-<input type="text" name="lastname" value="Mouse">
+Employee #:<br>
+<input type="text" name="emp_no" value="#">
+<br>
+Birth Date:<br>
+<input type="text" name="birth" value="#">
+<br>
+First Name:<br>
+<input type="text" name="firstname" value="#">
+<br>
+Last Name:<br>
+<input type="text" name="lastname" value="#">
+<br>
+Gender:<br>
+<input type="text" name="sex" value="#">
+<br>
+Hire Date:<br>
+<input type="text" name="hire" value="#">
 <br><br>
 <input type="submit" value="Submit">
+</form>
+<form action="index.php">               
+<input type="submit" value="Cancel">
 </form>

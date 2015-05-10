8091a628d02c80ba4957c89600e600648d1de05a
Arthur Chen <ac486@njit.edu>
2015-05-09 22:11:50 -0400

    added edit method

--- edit.php
+++ edit.php
@@ -1,4 +1,26 @@
-<form action="action_page.php">
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+include 'pages.php';
+      global $db;
+if (isset($_POST['edit'])){
+   $edit = "UPDATE employees SET birth_date = :bdate, first_name = :fname, last_name = :lname, hire_date = :hdate WHERE emp_no= :eid";
+$qry = $db->prepare($edit);
+
+$qry->bindParam(':eid', $_POST['emp_no'], PDO::PARAM_STR);
+$qry->bindParam(':bdate', $_POST['birth'], PDO::PARAM_STR);
+$qry->bindParam(':fname', $_POST['firstname'], PDO::PARAM_STR);
+$qry->bindParam(':lname', $_POST['lastname'], PDO::PARAM_STR);
+$qry->bindParam(':gen', $_POST['sex'], PDO::PARAM_STR);
+$qry->bindParam(':hdate', $_POST['hire'], PDO::PARAM_STR);
+$qry->execute();
+echo "Entry Submitted";
+exit();
+}
+?>
+
+<h2>Edit Employee Information</h2>
+<form action="" method = "POST">
 <br>
 Employee #:<br>
 <input type="text" name="emp_no" value="#">
@@ -19,6 +41,7 @@ Hire Date:<br>
 <input type="text" name="hire" value="#">
 <br><br>
 <input type="submit" value="Submit">
+<input type="hidden" value="edit">
 </form>
 <form action="index.php">               
 <input type="submit" value="Cancel">
